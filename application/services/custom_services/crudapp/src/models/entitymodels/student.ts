4b6d1d1bd37b2375f7f0a5af9e71d0c3bb9136a9
gepinfo <info@geppettosoftware.com>
2024-06-12 05:38:37 +0000

    code commited from geppetto!

---
+++ application/services/custom_services/crudapp/src/models/entitymodels/student.ts
@@ -0,0 +1,15 @@
+
+export interface student 
+{
+   created_date: { type: Date },
+   created_by: { type: String },
+   last_modified_by: { type: String },
+   last_modified_date: { type: Date },
+   name: { type: String },
+   age: { type: Number },
+   rollno: { type: String },
+   cls: { type: String }
+}
+
+
+
---
+++ application/services/default_services/systemcredentialmanager/src/credentials/securecredentials.ts
@@ -0,0 +1,4 @@
+export let credentials:any = {
+    DB_AUTH_reactjsapp_2296: 'mongodb://admin:password@',
+    DB_COLL_reactjsapp_2296: ':27017/reactjsapp_2296?authSource=admin'
+}
